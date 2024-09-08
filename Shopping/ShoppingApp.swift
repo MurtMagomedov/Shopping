de75e165e61f363cd1a2067ab9ee85c0cc82c9be
Murtazali <m.magomedov.a@mail.ru>
2024-09-08 12:56:10 +0300

    Initial Commit

---
+++ Shopping/ShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingApp.swift
+//  Shopping
+//
+//  Created by Муртазали Магомедов on 08.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
